952bd6ae545838deb9f0e9d7e4a1e6d1be49c9d3
PGrafkin <pavgra@gmail.com>
2019-06-14 17:23:00 -0400

    Initial commit

---
+++ proxy-svr/src/main/java/org/ohdsi/proxysrv/Application.java
@@ -0,0 +1,24 @@
+package org.ohdsi.proxysrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class Application {
+
+    public static void main(String[] args) {
+
+        // TODO:
+        // make faster registration of services for development profile
+        // https://github.com/spring-cloud/spring-cloud-netflix/issues/373#issuecomment-110331739
+
+        // TODO:
+        // when laptop goes into sleep and then wakes up - services do not work
+
+        SpringApplication.run(Application.class, args);
+    }
+}
