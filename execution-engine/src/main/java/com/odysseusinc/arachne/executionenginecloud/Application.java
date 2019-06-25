a29ec6888696452b9822d453a886ff3f19d4e4db
Pavel Grafkin <pavgra@gmail.com>
2019-06-25 15:34:19 +0300

    Wrapped core OHDSI components into Spring Cloud. Initial commit (#1)

---
+++ execution-engine/src/main/java/com/odysseusinc/arachne/executionenginecloud/Application.java
@@ -0,0 +1,25 @@
+package com.odysseusinc.arachne.executionenginecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@ComponentScan(
+        basePackages = {
+                "com.odysseusinc.arachne.executionenginecloud",
+                "com.odysseusinc.arachne.executionengine"
+        }
+)
+public class Application {
+
+    public static void main(String[] args) {
+
+        // TODO:
+        System.setProperty("server.port", "0");
+        System.setProperty("server.ssl.enabled", "false");
+
+        SpringApplication.run(Application.class, args);
+    }
+}
