76a6ee81bc7a42cedfa6baf0db44f18e005ebfd2
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2019-09-26 13:47:10 +0200

    added hive

--- execution-engine/src/main/java/com/odysseusinc/arachne/executionenginecloud/Application.java
+++ execution-engine/src/main/java/com/odysseusinc/arachne/executionenginecloud/Application.java
@@ -13,10 +13,10 @@ import org.springframework.context.annotation.PropertySource;
         }
 )
 /**
- * Note: When using default profile it's required to externalize properties needed to be overriden defaults
+ * Note: When using default profile it's required to externalize properties needed to be overridden defaults
  *  inherited from execution-engine dependency. At least <b>server.port</b> and <b>server.ssl.enabled</b>
- *  should be overriden directly by command line arguments or with additional config location.
- *  This is beacause spring determines properties loading order and external properties has prevalence over
+ *  should be overridden directly by command line arguments or with additional config location.
+ *  This is because spring determines properties loading order and external properties has prevalence over
  *  default application properties.
  *  Not a problem when running in docker container since docker externalize properties with environment variables.
  */
